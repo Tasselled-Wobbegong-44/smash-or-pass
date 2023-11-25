262440756fa64af0020c64f90590ac26e84d8a85
Jesse Rosengrant <jebsi@Jesses-MacBook-Air.local>
2023-11-25 09:37:28 -0500

    got cheeriotableparser working

--- server/scraper.js
+++ server/scraper.js
@@ -1,11 +1,24 @@
-import * as cheerio from 'cheerio';
-import * as fs from 'fs';
-import * as cheerioTable from 'cheerio-tableparser';
+// import * as cheerio from 'cheerio';
+const cheerio = require('cheerio');
+// import * as fs from 'fs';
+const fs = require('fs');
+// import * as cheerioTable from 'cheerio-tableparser';
+const cheerioTableParser = require('cheerio-tableparser');
+
+console.log('cheerioTable', cheerioTableParser);
 
 (async () => {
   const response = await fetch('https://ultimateframedata.com/stats');
   const rawHtml = await response.text();
   const $ = cheerio.load(rawHtml);
+
+  //testing table-parser
+  cheerioTableParser($);
+  const parseWeight = $('#weighttable').parsetable(true, true, true);
+  console.log(parseWeight);
+  const parseJumpHeight = $('#jumpheighttable').parsetable(true, true, true);
+  const parseDashRun = $('#dashandruntable').parsetable(true, true, true);
+  const parseGrabRange = $('#grabstable').parsetable(true, true, true);
 
   const weightTable = $('#weighttable');
   // const speedTable = $('#dashruntable');x
