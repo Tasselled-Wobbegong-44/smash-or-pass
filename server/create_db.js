39067feba27f6bcc6880a33795f1237dde06fe93
Jesse Rosengrant <jebsi@Jesses-MacBook-Air.local>
2023-11-25 15:39:56 -0500

    Refactored character extractor. Working on populating all DB tables now

--- server/create_db.js
+++ server/create_db.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 require('dotenv').config();
-const weights = require('./scraper.js');
-console.log('weight', weights);
+const stats = require('./scraper.js');
+console.log('weight', stats);
 
 const client = new Client({
   connectionString: process.env.PG_URI,
@@ -10,7 +10,14 @@ const client = new Client({
 client.connect();
 console.log('Client connection successful');
 
-// create weights table
+/* Initialize PSQL Tables
+decimal(precision, scale) 
+  precision = total significant digits on both sides of the decimal point
+  scale = number of digits on the right side of the decimal
+  e.g., decimal (4, 3) expects a number such as 1.805
+*/
+
+// Declare SQL query text that will be used to create blank tables
 const createTableScript = `
   CREATE TABLE IF NOT EXISTS weights (
     _id serial PRIMARY KEY,
@@ -18,13 +25,32 @@ const createTableScript = `
     weight integer
   );
 
-  CREATE TABLE IF NOT EXISTS weights (
+  CREATE TABLE IF NOT EXISTS ground_speed (
     _id serial PRIMARY KEY,
     name varchar,
-    weight integer
+    initial_dash decimal(4, 3),
+    run_speed decimal(4,3),
+    dash_frames integer,
+    pivot_dash_frames
+  );
+
+  CREATE TABLE IF NOT EXISTS jump_height (
+    _id serial PRIMARY KEY,
+    name varchar,
+    full_hop decimal(4, 2),
+    short_hop decimal(4, 2),
+    air_jump decimal(4, 2)
+  );
+
+  CREATE TABLE IF NOT EXISTS grab_range (
+    _id serial PRIMARY KEY,
+    name varchar,
+    grab_range
   );
   `;
 
+// Pass defined query text into the client query function
+// to create tables in Elephant SQL
 client.query(createTableScript, (err, result) => {
   if (err) {
     console.error('Error creating weight table:', err);
@@ -33,12 +59,15 @@ client.query(createTableScript, (err, result) => {
   }
 });
 
+// Initialize query script to insert values into newly created tables
+
+// ---- WEIGHT
 let insertWeightTableScript = `
   INSERT INTO weights (_id, name, weight) 
   VALUES
 `;
 
-weights.then((results) => {
+stats.parseWeight.then((results) => {
   results = JSON.parse(results);
   for (let i = 0; i < Object.keys(results).length; i++) {
     insertWeightTableScript += `(${i}, '${Object.keys(results)[i]}', ${
@@ -46,14 +75,90 @@ weights.then((results) => {
     }),`;
   }
 
-  // remove trailing comma
-  insertWeightTableScript = insertWeightTableScript.slice(0, -1);
-
-  console.log('full script: ', insertWeightTableScript);
+  insertWeightTableScript = insertWeightTableScript.slice(0, -1); // remove trailing comma
 
   client.query(insertWeightTableScript, (err, result) => {
     if (err) {
       console.error('Error inserting to weights table:', err);
+    } else {
+      console.log('Table populated successfully:', result);
+    }
+  });
+});
+
+// ---- GROUND SPEED
+let insertGroundSpeedTableScript = `
+  INSERT INTO weights (_id, name, weight) 
+  VALUES
+`;
+
+stats.parseWeight.then((results) => {
+  results = JSON.parse(results);
+  for (let i = 0; i < Object.keys(results).length; i++) {
+    insertGroundSpeedTableScript += `(${i}, '${Object.keys(results)[i]}', ${
+      Object.values(results)[i]
+    }),`;
+  }
+
+  insertGroundSpeedTableScript = insertGroundSpeedTableScript.slice(0, -1); // remove trailing comma
+
+  client.query(insertGroundSpeedTableScript, (err, result) => {
+    if (err) {
+      console.error('Error inserting to GroundSpeed table:', err);
+    } else {
+      console.log('Table populated successfully:', result);
+    }
+  });
+});
+
+// ---- JUMP HEIGHT
+let insertJumpHeightTableScript = `
+  INSERT INTO jump_height (_id, name, full_hop, short_hop, air_jump) 
+  VALUES
+`;
+
+stats.parseJumpHeight.then((results) => {
+  results = JSON.parse(results);
+  for (let i = 0; i < Object.keys(results).length; i++) {
+    insertWeightTableScript += `(${i}, '${Object.keys(results)[i]}', ${
+      Object.values(results)[i]
+    }),`;
+  }
+
+  insertJumpHeightTableScript = insertJumpHeightTableScript.slice(0, -1); // remove trailing comma
+
+  console.log('full script: ', insertJumpHeightTableScript);
+
+  client.query(insertJumpHeightTableScript, (err, result) => {
+    if (err) {
+      console.error('Error inserting to JumpHeights table:', err);
+    } else {
+      console.log('Table populated successfully:', result);
+    }
+  });
+});
+
+// ---- GRAB RANGE
+let insertGrabTableScript = `
+  INSERT INTO grab_range (_id, name, grab_range) 
+  VALUES
+`;
+
+stats.parseGrab.then((results) => {
+  results = JSON.parse(results);
+  for (let i = 0; i < Object.keys(results).length; i++) {
+    insertGrabTableScript += `(${i}, '${Object.keys(results)[i]}', ${
+      Object.values(results)[i]
+    }),`;
+  }
+
+  insertGrabTableScript = insertGrabTableScript.slice(0, -1); // remove trailing comma
+
+  console.log('full script: ', insertGrabTableScript);
+
+  client.query(insertGrabTableScript, (err, result) => {
+    if (err) {
+      console.error('Error inserting to GrabRange table:', err);
     } else {
       console.log('Table populated successfully:', result);
     }
--- server/scraper.js
+++ server/scraper.js
@@ -5,28 +5,29 @@ const fs = require('fs');
 // import * as cheerioTable from 'cheerio-tableparser';
 const cheerioTableParser = require('cheerio-tableparser');
 
-// console.log('cheerioTable', cheerioTableParser);
-const weightTable = {};
-const jumpTable = {};
-const speedTable = {};
-const grabTable = {};
-
+//declare async function, 'scrape'
 async function scrape(id) {
   //i: tableid(string)
   //o: object(table)
 
+  //fetch url of site we want
   const response = await fetch('https://ultimateframedata.com/stats');
+  //convert response to rawHtml text
   const rawHtml = await response.text();
+  //load rawHtml into cheerio
   const $ = cheerio.load(rawHtml);
 
   //testing table-parser
+  //passes loaded cheerio variable into table parser
+  // See documentation on cheerio-tableparser: https://www.npmjs.com/package/cheerio-tableparser
   cheerioTableParser($);
+  //somehow this returns the tables we want
   const parseTable = $(id).parsetable(true, true, true);
 
   return parseTable;
 }
 
-scrape();
+//array of all the characters we need
 const neededCharacters = [
   'Donkey Kong',
   'Mario',
@@ -53,46 +54,69 @@ const neededCharacters = [
   'Mr. Game & Watch',
   'Luigi',
 ];
-// const parseWeight = await scrape()
-// console.log(parseWeight)
 
-const weight = [];
 //helper function
-const characterExtractor = (arr) => {
+const characterExtractor = (data) => {
   //i: nested array
   //o: new nested array
-  console.log(arr.length);
 
-  //length of array determines number of nested arrays
-  const obj = {};
-
-  for (let i = 0; i < arr[1].length; i++) {
-    if (neededCharacters.includes(arr[1][i])) {
-      const character = arr[1][i];
-      const weight = arr[2][i];
-
-      obj[character] = weight;
+  //create empty buckets array
+  let result = [];
+  //populate buckets array with empty arrays for the length of our input array
+  data.forEach((element) => {
+    result.push([]);
+  });
+  //iterate through data array
+  for (let i = 0; i < data.length; i++) {
+    //if 0th index position of nested array is 'Rank'
+    if (data[i][0] === 'Rank') {
+      //slice it from the data array
+      data = data.slice(1);
+      //slice the result array as well to keep lengths consistent
+      result = result.slice(0, result.length - 1);
+    }
+    //otherwise 0th position will be 'Character'
+    //iterate through character array
+    for (let j = 0; j < data[0].length; j++) {
+      //if needed characters array contains element
+      //push that element along with corresponding indexes in other arrays into result
+      if (neededCharacters.includes(data[0][j])) result[i].push(data[i][j]);
     }
   }
-  return JSON.stringify(obj);
+
+  //return result
+  return result;
 };
 
-// console.log(parseWeight)
-
+//in order to get our table data we pass the style tags for the tables we want into scrape
+//in each of these functions we utilize a helper function to sort the data we want
 const parseWeight = scrape('#weighttable').then((data) => {
+  // console.log(data)
   const result = characterExtractor(data);
   // return result;
-  console.log(result);
+  // console.log(result);
   return result;
 });
 
-const parseDashRun = scrape('#dashandruntable').then((data) => {
+const parseGroundSpeed = scrape('#dashandruntable').then((data) => {
+  console.log(data);
   const result = characterExtractor(data);
+  return result;
 });
 
-// const parseHeight = scrape('#jumpheighttable').then((data) => {
-//   const result = characterExtractor(data);
-//   return result;
-// });
+const parseJumpHeight = scrape('#jumpheighttable').then((data) => {
+  const result = characterExtractor(data);
+  return result;
+});
 
-module.exports = parseWeight;
+const parseGrab = scrape('#grabstable').then((data) => {
+  const result = characterExtractor(data);
+  return result;
+});
+
+module.exports = {
+  parseWeight,
+  parseGroundSpeed,
+  parseJumpHeight,
+  parseGrab,
+};
